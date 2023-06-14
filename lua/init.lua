f77925d7eeb212beb3a30af84998d7203d8939cf
William Drescher <wilkua2@gmail.com>
2023-06-14 11:09:25 -0400

    Add packer config to startup call

--- lua/init.lua
+++ lua/init.lua
@@ -160,7 +160,7 @@ end
 
 local packer_bootstrap = ensure_packer()
 
-return require('packer').startup(function(use)
+return require('packer').startup({function(use)
     use 'wbthomason/packer.nvim'
 
     use {
@@ -278,4 +278,15 @@ return require('packer').startup(function(use)
     if packer_bootstrap then
         require('packer').sync()
     end
-end)
+end,
+config = {
+    profile = {
+        enable = false,
+        threshold = 10,
+    },
+    display = {
+        open_fn = function()
+            return require('packer.util').float { border = 'single' }
+        end,
+    },
+}})
