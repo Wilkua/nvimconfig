ef174669df92f55f4e73e5f71680131fd595afa9
William Drescher <wilkua2@gmail.com>
2023-11-15 23:31:43 -0500

    Better LSP for Neovim Lua

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -2,6 +2,13 @@ return {
     'neovim/nvim-lspconfig',
     dependencies = {
         {
+            'folke/neoconf.nvim',
+            cmd = 'Neoconf',
+            config = true,
+        },
+        { 'folke/neodev.nvim', opts = {} },
+        'mason.nvim',
+        {
             'williamboman/mason-lspconfig.nvim',
             cmd = { 'LspInstall', 'LspUninstall' },
             config = true,
