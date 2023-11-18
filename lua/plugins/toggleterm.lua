9ae869bec17469e2c23dd018383f81928bbe1816
William Drescher <wilkua2@gmail.com>
2023-11-18 00:27:43 -0500

    Reconfigure ToggleTerm

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -1,6 +1,9 @@
 return {
     'akinsho/toggleterm.nvim',
     version = '*',
-    cmd = { 'ToggleTerm' },
-    config = true,
+    cmd = { 'ToggleTerm', 'TermExec' },
+    opts = {
+        size = 30,
+        shell = vim.o.shell == 'cmd.exe' and 'pwsh.exe' or vim.o.shell,
+    },
 }
