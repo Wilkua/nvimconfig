80bb7e0a524451928900d089fa750d6270b62a6e
William Drescher <wilkua2@gmail.com>
2023-12-05 19:42:09 -0500

    Remove preconfigured null-ls sources

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -3,11 +3,7 @@ return {
     config = function()
         local null_ls = require 'null-ls'
         null_ls.setup {
-            sources = {
-                null_ls.builtins.diagnostics.eslint_d,
-                null_ls.builtins.diagnostics.php,
-                null_ls.builtins.formatting.rustfmt,
-            },
+            sources = {},
         }
     end,
 }
