3c4f74d100c8d0a845667dd8b0681dace373fb18
William Drescher <wilkua2@gmail.com>
2024-12-18 13:52:52 -0500

    update treesitter config for context comment string

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,40 +1,50 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-        'JoosepAlviste/nvim-ts-context-commentstring',
-        'nvim-treesitter/nvim-treesitter-textobjects',
-        -- windwp/nvim-ts-autotag/issues/125
-        { 'windwp/nvim-ts-autotag', opts = { autotag = { enable_close_on_slash = false } } },
-    },
-    build = ':TSUpdate',
-    cmd = {
-        'TSBufDisable', 'TSBufEnable', 'TSBufToggle',
-        'TSDisable', 'TSEnable', 'TSToggle',
-        'TSInstall', 'TSInstallInfo', 'TSInstallSync',
-        'TSModuleInfo', 'TSUninstall', 'TSUpdate',
-        'TSUpdateSync',
-    },
-    opts = {
-        ensure_installed = {
-            'c', 'lua', 'rust', 'json',
-            'html', 'javascript', 'typescript'
+    {
+        'windwp/nvim-ts-autotag',
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
+        opts = {
+            opts = {
+                enable_close = true,
+                enable_rename = true,
+                enable_close_on_slash = false
+            },
         },
-        sync_install = false,
-        ignore_install = {},
-        highlight = {
-            enable = true,
-            additional_vim_regex_highlighting  = false,
-        },
-        autotag = { enable = true },
-        indent = { enable = true },
-        context_commentstring = { enable = true },
-        incremental_selection = { enable = true },
     },
-    init = function()
-        vim.g.skip_ts_context_commentstring_module = true
-    end,
-    config = function(_, opts)
-        require 'nvim-treesitter'
-        require('nvim-treesitter.configs').setup(opts)
-    end,
+    {
+        'nvim-treesitter/nvim-treesitter',
+        dependencies = {
+            'JoosepAlviste/nvim-ts-context-commentstring',
+            'nvim-treesitter/nvim-treesitter-textobjects',
+        },
+        build = ':TSUpdate',
+        cmd = {
+            'TSBufDisable', 'TSBufEnable', 'TSBufToggle',
+            'TSDisable', 'TSEnable', 'TSToggle',
+            'TSInstall', 'TSInstallInfo', 'TSInstallSync',
+            'TSModuleInfo', 'TSUninstall', 'TSUpdate',
+            'TSUpdateSync',
+        },
+        opts = {
+            ensure_installed = {
+                'c', 'html', 'java', 'javascript', 'json', 'lua',
+                'rust', 'typescript', 'vim', 'vimdoc',
+            },
+            sync_install = false,
+            ignore_install = {},
+            highlight = {
+                enable = true,
+                additional_vim_regex_highlighting = false,
+            },
+            indent = { enable = true },
+            context_commentstring = { enable = true },
+            incremental_selection = { enable = true },
+        },
+        init = function()
+            vim.g.skip_ts_context_commentstring_module = true
+        end,
+        config = function(_, opts)
+            require 'nvim-treesitter'
+            require('nvim-treesitter.configs').setup(opts)
+        end,
+    },
 }
